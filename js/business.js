0637c4fcc52bc460c83343d11c60ad7053be82d8
Taryn <taryn@sugarpush.com.au>
2018-03-12 09:42:33 +1100

    Finished connecting DOM interaction to logic

--- js/business.js
+++ js/business.js
@@ -3,31 +3,54 @@ console.log("connected business");
 const waysToWin = ["123", "456", "789", "147", "258", "369", "159", "357"];
 
 let available = [1, 2, 3, 4, 5, 6, 7, 8, 9]; // plays available
-let currentPlayer;
+
 let current;
 let p1Played = []; // plays made
 let p2Played = []; // plays made
+let currentPlayed;
 let win = false;
 
 let p1; // player one
 let p2; // player two
 let brandNewCurrent;
+let playerName;
+let winnerBanner;
+let drawBanner;
+let currentWins;
+let p1Wins = 0;
+let p2Wins = 0;
+
+
 
 
 
 const findPlayer = function(currentPlayer) {
-  if (currentPlayer === 'p1') {
+  // console.log("start of findPlayer currentPlayer = " + currentPlayer);
+  if (currentPlayer === 0) { //If current player is 0 (player 1) then currentPlayed is p1Played, currentName is Player one.
     currentPlayed = p1Played;
-  } else if (currentPlayer === 'p2') {
+    playerName = "Player one";
+    icon = "O";
+    pClass = "o";
+    currentWins = p1Wins; // TODO: currentWins should be p1Wins but this doesn't work yet!
+  } else if (currentPlayer === 1) { //If current player is 1 (player 2) then currentPlayed is p2Played, current name is Player two.
     currentPlayed = p2Played;
+    playerName = "Player two";
+    icon = "X";
+    pClass = "x";
+    currentWins = p2Wins; // TODO: currentWins should be p2Wins but this doesn't work yet!
   }
+  // console.log(currentPlayed);
+logPlays(currentPlay); //Call log plays function
+console.log("end of findPlayer currentPlayer = " + currentPlayer);
 }
 
 
 const logPlays = function(currentPlay) {
-currentPlayed.push(currentPlay); //push currentPlay into p Played array
-console.log(p1Played);
-console.log(p2Played);
+currentPlayed.push(currentPlay); //push currentPlay into p1 or p2 Played array
+// console.log("log plays p1Played = " + p1Played);
+// console.log("log plays p2Played = " + p2Played);
+// console.log("log plays currentPlayed = " + currentPlayed)
+// console.log("log plays currentPlayer = " + currentPlayer);
 
 for (var i = available.length - 1; i >= 0; i--) { //loop through available until a match is found, then splice it out so it's removed
     if (available[i] === currentPlay) {
@@ -36,42 +59,57 @@ for (var i = available.length - 1; i >= 0; i--) { //loop through available until
     }
 }
 // console.log(available);
-findWinner(currentPlayed);
+findWinner(currentPlayed); // call findWinner function
 }
 
 
 const findWinner = function(currentPlayed) {
-  currentPlayed.sort();
+  currentPlayed.sort(); //sort currentPlayed array into order
   // console.log(currentPlayed);
-  let currentPlayedString = currentPlayed.toString();
+  let currentPlayedString = currentPlayed.toString(); //change it to a string
   // console.log(currentPlayedString);
-  let current = currentPlayedString.replace(/,/g, "")
+  let current = currentPlayedString.replace(/,/g, "") //remove the commas
   // console.log(current);
 
-  for (var i = waysToWin.length -1; i >= 0; i--) {
+  for (var i = waysToWin.length -1; i >= 0; i--) { // loop through each possible way to win, and if a match found return true
     if (waysToWin[i] === current) {
-      console.log(`match found`);
+      // console.log(`waysToWin - match found`);
       win = true;
-      return;
+
+      winnerBanner = $(`<p class="banner"> ${ playerName } wins! <a href="#">Try again?</a></p>`); //create a new paragraph saying which player won
+      $('.gameboard').append(winnerBanner) // append to DOM after gameboard
+      currentWins = currentWins + 1; // TODO: make p1Wins or p2Wins + 1 - THIS DOESN'T WORK
+      $('#one, #two, #three, #four, #five, #six, #seven, #eight, #nine').off('click'); //disable gameboard - game is over
     }
   }
 
-  if ((win !== true) && (available.length === 0)) {
-      console.log('draw')
+  if ((win !== true) && (available.length === 0)) { // if no match is found and no plays are left, create a new paragraph declaring the game a draw and append to DOM after gameboard.
+      console.log('waysToWin - draw')
+      winnerBanner = $(`<p class="banner"> It's a draw. <a href="#">Try again?</a></p>`);
+      $('.gameboard').append(winnerBanner)
+      currentWins = currentWins + 1;// TODO: make p1Wins or p2Wins + 1 - THIS DOESN'T WORK
+      $('#one, #two, #three, #four, #five, #six, #seven, #eight, #nine').off('click'); //disable gameboard - game is over
     }
 
-  nextPlayer();
+  nextPlayer(); //Call nextPlayer
 };
 
-const nextPlayer = function() {
-  if (currentPlayer === "p1") {
-    currentPlayer = "p2";
-  } else if (currentPlayer === "p2") {
-  currentPlayer = "p1";
+const nextPlayer = function() { // If current player is 0 (player 1), make current player 1 (player 2) and vice versa.
+  if (currentPlayer === 0) {
+    currentPlayer = 1;
+    // $('#p2-indicator').css('opacity', '1');
+  } else {
+  currentPlayer = 0;
+  // $('#p1-indicator').css('opacity', '1');
   }
-  // console.log(currentPlayer);
-  game();
+  // console.log("nextPlayer currentPlayer = " + currentPlayer);
+
+
 };
+
+// const nextGame = function() { TODO: Function to clear gameboard and start again
+//
+// };
 
 // findPlayer('p1', 1)
 // findPlayer('p2', 5)
--- js/main.js
+++ js/main.js
@@ -1,97 +1,118 @@
 console.log("connected main");
 let result;
 let currentPlay;
-
 let icon;
+let pClass;
+let currentPlayer;
 
 
 
 $(document).ready(function() {
-currentPlayer = "p1"
-
 
 game();
+
 });
 
 let game = function() {
-  findPlayer(currentPlayer);
-  if (currentPlayer === "p1") {
-    icon = "X";
-  } else if (currentPlayer === "p2") {
-    icon = "O";
-  }
+  currentPlayer = 0;
+
   click();
 }
 
 
 let click = function() {
+console.log("click currentPlayer = " + currentPlayer);
+  if (currentPlayer === 0) {
+    console.log('this is player 0' +currentPlayer);
+    icon = "X";
+    pClass = "x";
+  };
+
   $('#one').on('click', function () {
     currentPlay = 1;
-    let result = $(`<p> ${ icon } </p>`);
-    $('.parone').append(result)
+    let result = $(`<p class="${ pClass }"> ${ icon } </p>`);
+    $('.parone').append(result);
+    $('#one').off('click');
 
     // if (currentPlayer === "p1") {
     //   $('#one > p').addClass('p1-indicator')
     // } else if (currentPlayer === "p2") {
     //   $('#one > p').addClass('p2-indicator')
     // }
+    //
+    // logPlays(currentPlay);
 
-    logPlays(currentPlay);
+    //
+    // $('#one').on('click', function(event) {
+    //   event.preventDefault();
+    // })
+    findPlayer(currentPlayer);
+
+
   });
 
   $('#two').on('click', function () {
     currentPlay = 2;
-    let result = $(`<p> ${ icon } </p>`);
+    let result = $(`<p class="${ pClass }"> ${ icon } </p>`);
     $('.partwo').append(result);
-    logPlays(currentPlay);
+    findPlayer(currentPlayer);
+    $('#two').off('click');
   });
 
   $('#three').on('click', function () {
     currentPlay = 3;
-    let result = $(`<p> ${ icon } </p>`);
+    let result = $(`<p class="${ pClass }"> ${ icon } </p>`);
     $('.parthree').append(result);
-    logPlays(currentPlay);
+    findPlayer(currentPlayer);
+    $('#three').off('click');
   });
 
   $('#four').on('click', function () {
     currentPlay = 4;
-    let result = $(`<p> ${ icon } </p>`);
+    let result = $(`<p class="${ pClass }"> ${ icon } </p>`);
     $('.parfour').append(result);
-    logPlays(currentPlay);
+    findPlayer(currentPlayer);
+    $('#four').off('click');
   });
 
   $('#five').on('click', function () {
     currentPlay = 5;
-    let result = $(`<p> ${ icon } </p>`);
+    let result = $(`<p class="${ pClass }"> ${ icon } </p>`);
     $('.parfive').append(result);
-    logPlays(currentPlay);
+    findPlayer(currentPlayer);
+    $('#five').off('click');
   });
 
   $('#six').on('click', function () {
     currentPlay = 6;
-    let result = $(`<p> ${ icon } </p>`);
+    let result = $(`<p class="${ pClass }"> ${ icon } </p>`);
     $('.parsix').append(result);
-    logPlays(currentPlay);
+    findPlayer(currentPlayer);
+    $('#six').off('click');
   });
 
   $('#seven').on('click', function () {
     currentPlay = 7;
-    let result = $(`<p> ${ icon } </p>`);
+    let result = $(`<p class="${ pClass }"> ${ icon } </p>`);
     $('.parseven').append(result);
-    logPlays(currentPlay);
+    findPlayer(currentPlayer);
+    $('#seven').off('click');
   });
 
   $('#eight').on('click', function () {
     currentPlay = 8;
-    let result = $(`<p> ${ icon } </p>`);
+    let result = $(`<p class="${ pClass }"> ${ icon } </p>`);
     $('.pareight').append(result);
-    logPlays(currentPlay);
+    findPlayer(currentPlayer);
+    $('#eight').off('click');
   });
 
   $('#nine').on('click', function () {
     currentPlay = 9;
-    let result = $(`<p> ${ icon } </p>`);
+    let result = $(`<p class="${ pClass }"> ${ icon } </p>`);
     $('.parnine').append(result);
-    logPlays(currentPlay);
+    findPlayer(currentPlayer);
+    $('#nine').off('click');
   });
+
 };
