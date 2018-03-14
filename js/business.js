3cfbab6e323185c689050fd3d8eb4c862a93c428
Taryn <taryn@sugarpush.com.au>
2018-03-15 09:10:13 +1100

    Changed logic again

--- js/business.js
+++ js/business.js
@@ -1,162 +1,123 @@
 console.log("connected business");
 
 let board = {
-  1: "",
-  2: "",
-  3: "",
-  4: "",
-  5: "",
-  6: "",
-  7: "",
-  8: "",
-  9: ""
+  A1: "",
+  A2: "",
+  A3: "",
+  B1: "",
+  B2: "",
+  B3: "",
+  C1: "",
+  C2: "",
+  C3: ""
 };
 
-let x = false;
-let o = false;
-let draw = false;
-// let plays = ["X", "X", "X", "O", "O", "X", "O", "_", "_"];
-
+let waysToWin = [" X  X  X ", " O  O  O "];
 let available = [1, 2, 3, 4, 5, 6, 7, 8, 9]; // plays available
-
-let current;
-// let p1Played = []; // plays made
-// let p2Played = []; // plays made
-let currentPlayed;
-let win = false;
-
-let p1; // player one
-let p2; // player two
+let match = [];
+let matchString;
+let winner;
 let playerName;
 let winnerBanner;
-let currentWins;
 let p1Wins = 0;
 let p2Wins = 0;
 
 
-
-
 const findPlayer = function(currentPlayer) {
   if (currentPlayer === 0) { //If current player is 0 (player 1) then currentPlayed is p1Played, currentName is Player one.
-    // currentPlayed = p1Played;
     playerName = p1Name;
     icon = "O";
     pClass = "o";
   } else if (currentPlayer === 1) { //If current player is 1 (player 2) then currentPlayed is p2Played, current name is Player two.
-    // currentPlayed = p2Played;
     playerName = p2Name;
     icon = "X";
     pClass = "x";
   }
-  logPlays();
-// logPlays(currentPlay); //Call log plays function
+  findMatch(); //Call log plays function
 };
-
-const logPlays = function() {
-  for (var i = available.length - 1; i >= 0; i--) { //loop through available until a match is found, then splice it out so it's removed
-    if (available[i] === currentPlay) {
-      available.splice(i, 1);
-
-    }
-  }
-  console.log(available);
-  findMatch(); // call findWinner function
-};
-
 
 const findMatch = function(){
-  console.log(x);
-  console.log(o);
-  if ((board[1] === `X`) && (board[2] === `X`) && (board[3] === `X`)) {
-    x = true;
-  }
+  for (var i = 0; i < waysToWin.length; i++) {
+    //HORIZONTAL AND VERTICAL WINS
+    if (($('*[data-column="A"]').text()) === (waysToWin[i])) {
+      match.push(waysToWin[i]);
+      matchString = match.toString();
+      findWin();
+    } else if (($('*[data-column="B"]').text()) === (waysToWin[i])) {
+      match.push(waysToWin[i]);
+      matchString = match.toString();
+      findWin();
+    } else if (($('*[data-column="C"]').text()) === (waysToWin[i])) {
+      match.push(waysToWin[i]);
+      matchString = match.toString();
+      findWin();
+    } else if (($('*[data-row="1"]').text()) === (waysToWin[i])) {
+      match.push(waysToWin[i]);
+      matchString = match.toString();
+      findWin();
+    } else if (($('*[data-row="2"]').text()) === (waysToWin[i])) {
+      match.push(waysToWin[i]);
+      matchString = match.toString();
+      findWin();
+    } else if (($('*[data-row="3"]').text()) === (waysToWin[i])) {
+      console.log('normal win!');
+      match.push(waysToWin[i]);
+      matchString = match.toString();
+      findWin();
+    }
 
+    //DIAGONALS
+    else if ((board.A1 === `X`) && (board.B2 === `X`) && (board.C3 === `X`)) {
+      matchString = " X  X  X ";
+      findWin();
+      break;
+    } else if ((board.A1 === `O`) && (board.B2 === `O`) && (board.C3 === `O`)) {
+      matchString = " O  O  O ";
+      findWin();
+      break;
+    } else if ((board.C1 === `X`) && (board.B2 === `X`) && (board.A3 === `X`)) {
+      matchString = " X  X  X ";
+      findWin();
+      break;
+    } else if ((board.C1 === `O`) && (board.B2 === `O`) && (board.A3 === `O`)) {
+      matchString = " O  O  O ";
+      findWin();
+      break;
+    }
 
-  if ((board[1] === `O`) && (board[2] === `O`) && (board[3] === `O`)) {
-    o = true;
+    //DRAW
+    else if (available.length === 0) {
+      console.count('draw!!! - evaluating twice');
+      winnerBanner = $(`<p class="banner"> It's a draw. <a href="#" class="reset">Try again?</a></p>`).fadeIn( "slow" );
+      $('.gameboard').after(winnerBanner);
+      $('td').off('click'); //disable gameboard - game is over
+      nextGame();
+      break;
+    }
   }
-
-  if ((board[4] === `X`) && (board[5] === `X`) && (board[6] === `X`)) {
-    x = true;
-  }
-  if ((board[4] === `O`) && (board[5] === `O`) && (board[6] === `O`)) {
-    o = true;
-  }
-
-  if ((board[7] === `X`) && (board[8] === `X`) && (board[9] === `X`)) {
-    x = true;
-  }
-  if ((board[7] === `O`) && (board[8] === `O`) && (board[9] === `O`)) {
-    o = true;
-  }
-
-  if ((board[1] === `X`) && (board[4] === `X`) && (board[7] === `X`)) {
-    x = true;
-  }
-  if ((board[1] === `O`) && (board[4] === `O`) && (board[7] === `O`)) {
-    o = true;
-  }
-
-  if ((board[2] === `X`) && (board[5] === `X`) && (board[8] === `X`)) {
-    x = true;
-  }
-  if ((board[2] === `O`) && (board[5] === `O`) && (board[8] === `O`)) {
-    o = true;
-  }
-
-  if ((board[3] === `X`) && (board[6] === `X`) && (board[9] === `X`)) {
-    x = true;
-  }
-  if ((board[3] === `O`) && (board[6] === `O`) && (board[9] === `O`)) {
-    o = true;
-  }
-
-  if ((board[1] === `X`) && (board[5] === `X`) && (board[9] === `X`)) {
-    x = true;
-  }
-  if ((board[1] === `O`) && (board[5] === `O`) && (board[9] === `O`)) {
-    o = true;
-  }
-
-  if ((board[3] === `X`) && (board[5] === `X`) && (board[7] === `X`)) {
-    x = true;
-  }
-  if ((board[3] === `O`) && (board[5] === `O`) && (board[7] === `O`)) {
-    o = true;
-  }
-
-  else if (available.length === 0) {
-    draw = true;
-  }
-  findWin();
+  nextPlayer();
 };
 
 const findWin = function() {
+  if (matchString === " X  X  X ") {
+    winner = p1Name;
+  } else if (matchString === " O  O  O ") {
+    winner = p2Name;
+  }
 
-  if (x) {
-    winnerBanner = $(`<p class="banner"> ${ playerName } wins! <a href="#" class="reset">Try again?</a></p>`).fadeIn( "slow" ); //create a new paragraph saying which player won
-    $('.gameboard').after(winnerBanner); // append to DOM after gameboard
-    $('#td1, #td2, #td3, #td4, #td5, #td6, #td7, #td8, #td9').off('click'); //disable gameboard - game is over
+  winnerBanner = $(`<p class="banner"> ${ winner } wins! <a href="#" class="reset">Try again?</a></p>`).fadeIn( "slow" ); //create a new paragraph saying which player won
+  $('.gameboard').after(winnerBanner); // append to DOM after gameboard
+  $('td').off('click'); //disable gameboard - game is over
+  if (winner === p1Name) {
     p1Wins = p1Wins + 1; // add 1 win to p1Wins
-    $('.p1 h2').text(`${ playerName }: ${ p1Wins } wins`)
-    nextGame();
+    $('.p1 h2').text(`${ playerName }: ${ p1Wins } wins`);
+    console.log('if winner!!! - evaluating twice');
+  } else {
+    console.log('else winner - evaluating twice');
+    p2Wins = p2Wins + 1; // add 1 win to p1Wins
+    $('.p2 h2').text(`${ playerName }: ${ p2Wins } wins`);
   }
-  if (o) {
-    winnerBanner = $(`<p class="banner"> ${ playerName } wins! <a href="#" class="reset">Try again?</a></p>`).fadeIn( "slow" ); //create a new `par`agraph saying which player won
-    $('.gameboard').after(winnerBanner); // append to DOM after gameboard
-    $('#td1, #td2, #td3, #td4, #td5, #td6, #td7, #td8, #td9').off('click'); //disable gameboard - game is over
-    p2Wins = p2Wins + 1; // add 1 win to p2Wins
-    $('.p2 h2').text(`${ playerName }: ${ p2Wins } wins`)
-    nextGame();
-  }
-  if ((draw) && (o === false) && (x === false)) {
-    winnerBanner = $(`<p class="banner"> It's a draw. <a href="#" class="reset">Try again?</a></p>`).fadeIn( "slow" );
-    $('.gameboard').after(winnerBanner);
-    $('#td1, #td2, #td3, #td4, #td5, #td6, #td7, #td8, #td9').off('click'); //disable gameboard - game is over
-    nextGame();
-
-  }
-  nextPlayer();
+  nextGame();
 };
 
 const nextPlayer = function() { // If current player is 0 (player 1), make current player 1 (player 2) and vice versa.
@@ -166,44 +127,31 @@ const nextPlayer = function() { // If current player is 0 (player 1), make curre
     $('.p1-indicator').css('opacity', '0'); // Turn opacity to 0 for p1 indicator
   } else {
     currentPlayer = 0;
-  $('.p1-indicator').css('opacity', '1'); // Turn opacity to 1 for p1 indicator
-  $('.p2-indicator').css('opacity', '0'); // Turn opacity to 0 for p2 indicator
+    $('.p1-indicator').css('opacity', '1'); // Turn opacity to 1 for p1 indicator
+    $('.p2-indicator').css('opacity', '0'); // Turn opacity to 0 for p2 indicator
   }
 };
 
 const nextGame = function() { // Function to clear gameboard and start again
-$('.reset').on('click', function() {
-  $('.x').remove();
-  $('.o').remove();
-  $('.banner').remove();
-  available = [1, 2, 3, 4, 5, 6, 7, 8, 9]; // plays available
-  board = {
-    1: "",
-    2: "",
-    3: "",
-    4: "",
-    5: "",
-    6: "",
-    7: "",
-    8: "",
-    9: ""
-  };
-  x = false;
-  o = false;
-  draw = false;
-  game();
-});
+  $('.reset').on('click', function() {
+    $('.x').remove();
+    $('.o').remove();
+    $('.banner').remove();
+    available = [1, 2, 3, 4, 5, 6, 7, 8, 9]; // plays available
+    board = {
+      A1: "",
+      A2: "",
+      A3: "",
+      B1: "",
+      B2: "",
+      B3: "",
+      C1: "",
+      C2: "",
+      C3: ""
+    };
+    match = [];
+    matchString;
+
+    click();
+  });
 };
-
-// LOCAL STORAGE
-
-// //check player score, if it is higher then top score then update top scorer name
-// function setHighScore(name, newScore) {
-//   let res = confirm("This is a new high score. Do you want to save this score ?");
-//   if (res === true)
-// {
-//   //save the winner score and name to local storage
-//   localStorage.highScore = newScore;
-//   localStorage.highScorerName = name;
-// }
-// }
--- js/main.js
+++ js/main.js
@@ -11,70 +11,65 @@ let flag;
 
 
 $(document).ready(function() {
-console.log(currentPlayer);
+  console.log(currentPlayer);
+
   $('#submit').on('click', function () {
     if ($('#p1Name').val()  === '') {
-      errorBanner = $(`<p class="error-banner">Player one, please add your name!</a></p>`).addClass('animated bounceIn')
+      errorBanner = $(`<p class="error-banner">Player one, please add your name!</a></p>`).addClass('animated bounceIn');
       $('.landing button').after(errorBanner);
-        return false;
+      return false;
     }
-
     if ($('#p2Name').val()  === '') {
-        errorBanner = $(`<p class="error-banner">Player two, please add your name!</a></p>`).fadeIn( "slow" );
-        $('.landing button').after(errorBanner);
-        return false;
+      errorBanner = $(`<p class="error-banner">Player two, please add your name!</a></p>`).fadeIn( "slow" );
+      $('.landing button').after(errorBanner);
+      return false;
     }
-
     p1Name = $('#p1Name').val();
     p2Name = $('#p2Name').val();
     $('.p1 h2').text(`${ p1Name }: 0 wins`);
     $('.p2 h2').text(`${ p2Name }: 0 wins`);
     landingToMain();
-
   });
+
   startingPlayer();
 });
 
+
+
 let startingPlayer = function() {
-  // $('.players p').fadeIn(1000).fadeOut(1000).fadeIn(1000);
 
   $('.p1').on('click', function () {
     currentPlayer = 0;
-    console.log(currentPlayer);
     $('.p2-indicator').css('opacity', '0'); // Turn opacity to 1 for p2 indicator
     $('.p1-indicator').css('opacity', '1'); // Turn opacity to 0 for p1 indicator
-    console.log(currentPlayer);
     $('.players p').css('visibility', 'hidden');
     $('.p1 h2').hover(function() {
       $(this).css('font-weight', '300');
-    })
+    });
     $('.p2 h2').hover(function() {
       $(this).css('font-weight', '300');
-    })
+    });
     $('.p1').off('click');
     $('.p2').off('click');
-    game();
+    click();
   });
 
   $('.p2').on('click', function () {
     currentPlayer = 1;
-    console.log(currentPlayer);
     $('.p2-indicator').css('opacity', '1'); // Turn opacity to 1 for p2 indicator
     $('.p1-indicator').css('opacity', '0'); // Turn opacity to 0 for p1 indicator
-    console.log(currentPlayer);
     $('.players p').css('visibility', 'hidden');
     $('.p1 h2').hover(function() {
       $(this).css('font-weight', '300');
-    })
+    });
     $('.p2 h2').hover(function() {
       $(this).css('font-weight', '300');
-    })
+    });
     $('.p1').off('click');
     $('.p2').off('click');
-    game();
+    click();
   });
 };
-
 
 let landingToMain = function() { // change display from landing page to main page page
   $('.landing').css('display', 'none');
@@ -83,24 +78,7 @@ let landingToMain = function() { // change display from landing page to main pag
 
 };
 
-
-
-let game = function() {
-  // currentPlayer = 0;
-  // $('.p1-indicator').css('opacity', '1'); // Start player one indicator opacity to 1 (visible)
-  console.log(currentPlayer);
-  click();
-
-};
-
-
-
-
-
-
-
 let click = function() {
-  console.log(currentPlayer);
   if (currentPlayer === 0) {
     icon = "X";
     pClass = "x";
@@ -108,88 +86,84 @@ let click = function() {
     icon = "O";
     pClass = "o";
   }
-  console.log(currentPlayer);
 
   $('#td1').on('click', function () {
-    currentPlay = 1;
+    available.pop();
     let result = $(`<p class="${ pClass }"> ${ icon } </p>`).fadeIn( "slow" );
     $('.par1').append(result);
-    board[1] = `${ icon }`;
+    board.A1 = `${ icon }`;
     findPlayer(currentPlayer);
     $('#td1').off('click');
-
-
-
   });
 
   $('#td2').on('click', function () {
-    currentPlay = 2;
+    available.pop();
     let result = $(`<p class="${ pClass }"> ${ icon } </p>`).fadeIn( "slow" );
     $('.par2').append(result);
-    board[2] = `${ icon }`;
+    board.A2 = `${ icon }`;
     findPlayer(currentPlayer);
     $('#td2').off('click');
   });
 
   $('#td3').on('click', function () {
-    currentPlay = 3;
+    available.pop();
     let result = $(`<p class="${ pClass }"> ${ icon } </p>`).fadeIn( "slow" );
     $('.par3').append(result);
-    board[3] = `${ icon }`;
+    board.A3 = `${ icon }`;
     findPlayer(currentPlayer);
     $('#td3').off('click');
   });
 
   $('#td4').on('click', function () {
-    currentPlay = 4;
+    available.pop();
     let result = $(`<p class="${ pClass }"> ${ icon } </p>`).fadeIn( "slow" );
     $('.par4').append(result);
-    board[4] = `${ icon }`;
+    board.B1 = `${ icon }`;
     findPlayer(currentPlayer);
     $('#td4').off('click');
   });
 
   $('#td5').on('click', function () {
-    currentPlay = 5;
+    available.pop();
     let result = $(`<p class="${ pClass }"> ${ icon } </p>`).fadeIn( "slow" );
     $('.par5').append(result);
-    board[5] = `${ icon }`;
+    board.B2 = `${ icon }`;
     findPlayer(currentPlayer);
     $('#td5').off('click');
   });
 
   $('#td6').on('click', function () {
-    currentPlay = 6;
+    available.pop();
     let result = $(`<p class="${ pClass }"> ${ icon } </p>`).fadeIn( "slow" );
     $('.par6').append(result);
-    board[6] = `${ icon }`;
+    board.B3 = `${ icon }`;
     findPlayer(currentPlayer);
     $('#td6').off('click');
   });
 
   $('#td7').on('click', function () {
-    currentPlay = 7;
+    available.pop();
     let result = $(`<p class="${ pClass }"> ${ icon } </p>`).fadeIn( "slow" );
     $('.par7').append(result);
-    board[7] = `${ icon }`;
+    board.C1 = `${ icon }`;
     findPlayer(currentPlayer);
     $('#td7').off('click');
   });
 
   $('#td8').on('click', function () {
-    currentPlay = 8;
+    available.pop();
     let result = $(`<p class="${ pClass }"> ${ icon } </p>`).fadeIn( "slow" );
     $('.par8').append(result);
-    board[8] = `${ icon }`;
+    board.C2 = `${ icon }`;
     findPlayer(currentPlayer);
     $('#td8').off('click');
   });
 
   $('#td9').on('click', function () {
-    currentPlay = 9;
+    available.pop();
     let result = $(`<p class="${ pClass }"> ${ icon } </p>`).fadeIn( "slow" );
     $('.par9').append(result);
-    board[9] = `${ icon }`;
+    board.C3 = `${ icon }`;
     findPlayer(currentPlayer);
     $('#td9').off('click');
   });
